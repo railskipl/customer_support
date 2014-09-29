d3d238c4676b341074573802e39f8364b2795628
atishkunalinfotech <atish.lewate@kunalinfotech.net>
2014-09-29 11:11:10 +0530

    updated recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,19 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6LfYtvoSAAAAAM1aB8TGdjM1aZWGWbgDxE67uunw'
-  config.private_key = '6LfYtvoSAAAAACZrIAM-8-1cIkP_THyi22RMAcvl '
-  #config.proxy = 'http://0.0.0.0.3000'
+if Rails.env == "development"
+	Recaptcha.configure do |config|
+	  config.public_key  = '6LfYtvoSAAAAAM1aB8TGdjM1aZWGWbgDxE67uunw'
+	  config.private_key = '6LfYtvoSAAAAACZrIAM-8-1cIkP_THyi22RMAcvl '
+	  #config.proxy = 'http://0.0.0.0.3000'
+	end
+elsif Rails.env == "production"
+	Recaptcha.configure do |config|
+	  config.public_key  = '6LdxGfsSAAAAAGqbURclTwDO_JZnquS0VQQ3HX56'
+	  config.private_key = '6LdxGfsSAAAAADYUG2YkJfW1oKK6YbjSm1DV_RuL'
+	  #config.proxy = 'http://0.0.0.0.3000'
+	end
+else
+	Recaptcha.configure do |config|
+	  config.public_key  = '6LfYtvoSAAAAAM1aB8TGdjM1aZWGWbgDxE67uunw'
+	  config.private_key = '6LfYtvoSAAAAACZrIAM-8-1cIkP_THyi22RMAcvl '
+	  #config.proxy = 'http://0.0.0.0.3000'
+	end
 end

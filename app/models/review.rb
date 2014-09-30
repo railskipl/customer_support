eb091eee3fbf07ae9a63326049ccb1efcae442e2
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-09-30 18:08:17 +0530

    changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -28,7 +28,7 @@ class Review < ActiveRecord::Base
 	
   def self.search(search)
     search_condition = "%" + search + "%"
-    find(:all, :conditions => ['title LIKE ? OR message LIKE ? OR review_type LIKE ? OR ticket_number LIKE ? ', search_condition, search_condition,search_condition,search_condition])
+    find(:all, :conditions => ['(title LIKE ? OR message LIKE ? OR review_type LIKE ? OR ticket_number LIKE ? ) && ispublished = ?', search_condition, search_condition,search_condition,search_condition,true])
   end
 
   def valid_date?

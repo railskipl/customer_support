4f50cb5cdcab993bc898536dfb1d0bf0e75f6c37
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-11-06 18:05:06 +0530

    comment

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -5,9 +5,17 @@ class Admin::CommentsController < AdminController
   def create
       @comment = @review.comments.new(comment_params)
       @comment.user_id = current_user.id
-      @comment.ispublished = true
+      if current_user.role == "jagent"
+        m = MonitorJagent.find_or_create_by_review_id(@review.id)
+        m.s_comment = true
+      else
+        @comment.ispublished = true
+      end
+    
       if @comment.save && !params["comment"]["supplier_id"].nil?
-        ReviewMailer.comment_mail(@review,@comment).deliver!
+        unless current_user.role == "jagent"
+          ReviewMailer.comment_mail(@review,@comment).deliver!
+        end
         redirect_to [:admin, @review], :notice => 'Thanks for your comment'
       else
         redirect_to [:admin, @review], :alert => 'Unable to add comment'
@@ -20,13 +28,19 @@ class Admin::CommentsController < AdminController
 
  def update
     @comment =  @review.comments.find(params[:id])
-    # if current_user.role == "jagent"
-    #   m = MonitorJagent.find_or_create_by_review_id(@review.id)
-    #   m.c_comment = true
-    # else 
-      @comment.ispublished = true
-    # end
+    if current_user.role == "jagent"
+      m = MonitorJagent.find_or_create_by_review_id(@review.id)
+      m.c_comment = true
+      m.save
+      @comment.modified_comment = params["comment"]["modified_comment"]
+    else 
+      @comment.modified_comment = params["comment"]["modified_comment"] if params["comment"]
+      @comment.ispublished = true if params[:commit] == "Publish"
+    end
     if @comment.save
+       unless current_user.role == "jagent"
+          ReviewMailer.comment_mail(@review,@comment).deliver!
+        end
       redirect_to [:admin,@review], :notice => 'Comment has sucessfully Saved.'
     else
       redirect_to [:admin,@review], :alert => 'Error in Comment publishing .'
@@ -40,6 +54,6 @@ class Admin::CommentsController < AdminController
   end
 
   def comment_params
-    params.require(:comment).permit(:title, :user_id, :review_id, :supplier_id)
+    params.require(:comment).permit(:title, :user_id, :review_id, :supplier_id,:modified_comment)
   end
 end

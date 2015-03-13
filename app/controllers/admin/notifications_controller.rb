7a62b949915a132384f275a2a5a4306dd0351505
kipl <kipl@kipl-Inspiron-620s.(none)>
2015-03-13 12:55:01 +0530

    minor changes and give permission to admin

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -3,6 +3,7 @@ class Admin::NotificationsController < AdminController
   def index
   	if current_user.is_agent?
 	  @notifications = current_user.agent_notifications.includes(comment: :review)
+	  @all_notifications = Notification.includes(comment: :review)
 	else
 	  @notifications = current_user.jagent_notifications.includes(comment: :review)
     end

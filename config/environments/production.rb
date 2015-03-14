ccd8bfb66567984703080316da07a2e95012df21
kipl <kipl@kipl-Inspiron-620s.(none)>
2015-03-14 16:31:58 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,16 +82,16 @@ Jmd::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
             :enable_starttls_auto => true,
-            :address => "smtp.gmail.com",
-            :port => 587,
-            :domain => "imap.gmail.com",
+            :address => "kun.kunalinfotech.net",
+            :port => 23,
+            :domain => "kun.kunalinfotech.net",
             :authentication => :login,
-            :user_name => "rubyrails9@gmail.com",
-            :password => "utmeter!@#02"
+            :user_name => "ankit@kunalinfotech.net",
+            :password => "ankipl123"
   }
 
  config.action_mailer.default_url_options = { :host => 'http://www.xemaxema.com/' }
 
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 end
 #http://69.25.137.192/

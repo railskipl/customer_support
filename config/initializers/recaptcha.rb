c73b81f56858b8a8900bd9a2b86a1c90112dd020
kipl <kipl@kipl-Inspiron-620s.(none)>
2015-03-17 12:07:00 +0530

    changes

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -6,8 +6,8 @@ if Rails.env == "development"
 	end
 elsif Rails.env == "production"
 	Recaptcha.configure do |config|
-	  config.public_key  = '6LdeoAMTAAAAAD6RFowAvp9Ok89MfdOlhlLdlBPx'
-	  config.private_key = '6LeOngMTAAAAAFeSBUm1znvdX0Snb7Tn8-7UzHL6'
+	  config.public_key  = '6LdxGfsSAAAAAGqbURclTwDO_JZnquS0VQQ3HX56'
+	  config.private_key = '6LdxGfsSAAAAADYUG2YkJfW1oKK6YbjSm1DV_RuL'
 	  #config.proxy = 'http://0.0.0.0.3000'
 	end
 else

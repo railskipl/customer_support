b288023aa6ae465838582d005d95a9079f7f3f37
kipl <kipl@kipl-Inspiron-620s.(none)>
2015-03-28 20:47:25 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Jmd::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  #config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 
   # Generate digests for assets URLs.
   config.assets.digest = true

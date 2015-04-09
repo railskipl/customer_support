dd64e8445ddc9bcefd20c9d922bbeabb07f54e6e
kipl <kipl@kipl-Inspiron-620s.(none)>
2015-04-09 16:23:49 +0530

    breadcrumb changes

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,11 +1,12 @@
 class PollsController < ApplicationController
 	def show
-		@active_tab = "polls-bloopers"
-		@poll = Poll.find(params[:id])
-   respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @poll }
-      format.json  { render :xml => @poll }
-    end
+	  @nav_tab = "reviews"
+	  @active_tab = "polls & bloopers"
+	  @poll = Poll.find(params[:id])
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @poll }
+        format.json  { render :xml => @poll }
+      end
 	end
 end

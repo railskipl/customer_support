d4119f8a06a7ded8c20f59e7b290d9970120f1e8
kipl <kipl@kipl-Inspiron-620s.(none)>
2015-04-20 12:07:56 +0530

    password

--- app/mailers/agent_mailer.rb
+++ app/mailers/agent_mailer.rb
@@ -1,8 +1,9 @@
 class AgentMailer < ActionMailer::Base
   default from: "admin@xemaxema.com"
 
-  def agent_mail(agent)
+  def agent_mail(agent,password)
     @agent = agent
+    @password = password
     mail(:to => @agent.email,
          :subject => "A message from Customer Support"
     )

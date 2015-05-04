ec805d85a9955c775a5b5c51c11a0b3c2f8c1b02
kipl <kipl@kipl-Inspiron-620s.(none)>
2015-05-04 17:50:11 +0530

    monitor agent

--- app/controllers/admin/monitor_jagents_controller.rb
+++ app/controllers/admin/monitor_jagents_controller.rb
@@ -2,6 +2,6 @@ class Admin::MonitorJagentsController <  AdminController
 
 	def index
 		@monitors = MonitorJagent.includes(review: :track_times).order('reviews.id desc')
-		@reviews = Review.where("agent_id = ? and archive = ? and ispublished = ?", current_user.id,false,false).order('id desc')
+		@reviews = Review.where("agent_id = ? || jagent_id = ? and archive = ? and ispublished = ?",current_user.id ,current_user.id,false,false).order('id desc')
 	end
 end

5463d6c521671c9da8437d41b30d26aef55f94d3
atishkunalinfotech <atish.lewate@kunalinfotech.net>
2015-05-19 11:04:42 +0530

    added s3 credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,11 +6,11 @@ CarrierWave.configure do |config|
   
   config.fog_credentials = {
       :provider               => 'AWS',       # required
-      :aws_access_key_id      => 'AKIAIWWQF5REKTC4ZYGQ',       # required
-      :aws_secret_access_key  => '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',       # required
+      :aws_access_key_id      => 'AKIAJHIYTFVASMQLCOUA',       # required
+      :aws_secret_access_key  => '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',       # required
    #   :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = 'devcsupport'                     # required
+    config.fog_directory  = 's3xema'                     # required
    # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
    config.fog_public     = true                                   # optional, defaults to true
    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
